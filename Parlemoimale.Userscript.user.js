097356bd83321a4c0b89468cd4971d91dea0fea1
halna_Tanaguru <fhalna@oceaneconsulting.com>
2017-11-25 00:31:54 +0100

    Add files via upload

---
+++ Parlemoimale.Userscript.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Parlemoimale
+// @namespace    https://gist.github.com/fhalna/207d4f32777031c00aa122121156a28c
+// @version      0.1
+// @description  Masque l'écriture inclusive pour les lecteurs d'écran
+// @author       Frédéric Halna
+// @match      *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.body.innerHTML = document.body.innerHTML.replace(/elles·/g, '<span aria-hidden="true">elles.</span>');
+    document.body.innerHTML = document.body.innerHTML.replace(/·ses/g, '<span aria-hidden="true">.ses</span>');
+    document.body.innerHTML = document.body.innerHTML.replace(/·te·/g, '<span aria-hidden="true">·te·</span>');
+    document.body.innerHTML = document.body.innerHTML.replace(/·es/g, '<span aria-hidden="true">·es</span>');
+    document.body.innerHTML = document.body.innerHTML.replace(/·e·/g, '<span aria-hidden="true">·e·</span>');
+    document.body.innerHTML = document.body.innerHTML.replace(/n·e/g, 'n<span aria-hidden="true">·e</span>');
+    document.body.innerHTML = document.body.innerHTML.replace(/·rice·/g, '<span aria-hidden="true">·rice·</span>');
+})();
